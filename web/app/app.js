b099848225952c07128a6fcf6193c2c5db0dc94d
Andrew Krawchyk <akrawchyk@gmail.com>
2017-03-19 23:16:46 -0400

    consistent folder names

--- web/app/app.js
+++ web/app/app.js
@@ -1,10 +1,10 @@
-import 'path'
+import path from 'path'
 import express from 'express'
-// import favicon from 'serve-favicon'
+import favicon from 'serve-favicon'
 
 const app = express()
 
-// app.use(favicon(path.join(__dirname, 'public/favicon.ico')))
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 app.get('/', (req, res) => {
   res.send('Hello burger\n')
