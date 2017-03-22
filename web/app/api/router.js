1cdc8651b2756ed35333b5dcf20aaf2eccedce2d
Andrew Krawchyk <akrawchyk@gmail.com>
2017-03-22 00:01:13 -0400

    async functions and connecting to couchbase

--- web/app/api/router.js
+++ web/app/api/router.js
@@ -1,15 +1,40 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import { createComment } from './beer'
 
 const router = express.Router()
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-router.get('/query', function (req, res) {
-  res.send('Hello from /query')
-})
+function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-router.post('/command', urlencodedParser, function (req, res) {
-  res.send('Hello from /command')
-})
+// FIXME catch errors for ALL async functions
+
+async function helloWorld(req, res, next) {
+  await timeout(1000)
+  res.json({
+    success: 'true'
+  })
+}
+
+async function command(req, res, next) {
+  await timeout(1000)
+
+  const body = req.body
+  const text = body.text || ''
+
+  // process incoming command
+  try {
+    const doc = await createComment(text)
+    res.json(doc)
+  } catch (err) {
+    res.status(500).send(err.message)
+  }
+}
+
+router
+  .get('/query', helloWorld)
+  .post('/command', urlencodedParser, command)
 
 export default router
